2071399ad458db5c38cbe28803acd8e3a05e2689
Hendra Permana <hynra@yahoo.com>
2017-04-13 12:44:05 +0700

    save methods

--- src/main/java/com/github/hynra/jsonsharedpreferences/JSONSharedPreferences.java
+++ src/main/java/com/github/hynra/jsonsharedpreferences/JSONSharedPreferences.java
@@ -3,6 +3,8 @@ package com.github.hynra.jsonsharedpreferences;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+
 /**
  * Created by hynra on 4/13/17.
  */
@@ -20,4 +22,18 @@ public class JSONSharedPreferences {
         mSharedPrefs = context.getSharedPreferences(mPreferencesName, Context.MODE_PRIVATE);
         mEditor = mSharedPrefs.edit();
     }
+
+    public void save(Object object){
+        String val = new Gson().toJson(object.getClass());
+        mEditor.putString(object.getClass().getCanonicalName(), val);
+    }
+
+    public void save(Object object, String values){
+        mEditor.putString(object.getClass().getCanonicalName(), values);
+    }
+
+    public void commit(){
+        mEditor.commit();
+    }
+
 }
