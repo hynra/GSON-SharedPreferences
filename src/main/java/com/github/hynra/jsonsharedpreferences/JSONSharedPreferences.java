b0119e996c961e39a5e1c8b4933e0e760767895d
Hendra Permana <hynra@yahoo.com>
2017-04-13 15:13:32 +0700

    throw exception

--- src/main/java/com/github/hynra/jsonsharedpreferences/JSONSharedPreferences.java
+++ src/main/java/com/github/hynra/jsonsharedpreferences/JSONSharedPreferences.java
@@ -2,11 +2,10 @@ package com.github.hynra.jsonsharedpreferences;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.util.Log;
-
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import org.json.JSONObject;
 
-import java.lang.reflect.Type;
 
 /**
  * Created by hynra on 4/13/17.
@@ -29,20 +28,38 @@ public class JSONSharedPreferences {
     public void save(Object object){
         String val = new Gson().toJson(object);
         mEditor.putString(object.getClass().getCanonicalName(), val);
+        commit();
     }
 
     public void save(Object object, String values){
         mEditor.putString(object.getClass().getCanonicalName(), values);
+        commit();
+    }
+
+
+    public SharedPreferences getSharedPreferences(){
+        return mSharedPrefs;
+    }
+
+
+    public void save(Object object, JSONObject values){
+        mEditor.putString(object.getClass().getCanonicalName(), values.toString());
+        commit();
     }
 
     public void commit(){
         mEditor.commit();
     }
 
-    public Object get(Object object){
-        String val = mSharedPrefs.getString(object.getClass().getCanonicalName(), "");
-        object = object.getClass();
-        object = new Gson().fromJson(val, (Class<Object>) object);
+    public Object get(Object object) throws Exception{
+        try {
+            String val = mSharedPrefs.getString(object.getClass().getCanonicalName(), "");
+            object = object.getClass();
+            object = new Gson().fromJson(val, (Class<Object>) object);
+        }catch (JsonSyntaxException exception){
+            throw new JsonSyntaxException(exception);
+        }
+
         return object;
     }
 
